e8aa7fee1253ee2623b70d97a81440a7da219ece
Ian Halpern <ian@payload.co>
2019-12-24 11:25:13 -0500

    fixed request to clone request before flattening

--- payload/arm/request.py
+++ payload/arm/request.py
@@ -4,6 +4,7 @@ import sys
 
 import payload
 import requests
+import copy
 
 from ..utils import (convert_fieldmap, map_attrs, map_object,
                      nested_qstring_keys, object2data)
@@ -30,8 +31,8 @@ class ARMRequest(object):
         auth     = (payload.api_key, '')
         files    = {}
 
-        flat_data = nested_qstring_keys(json)
-        for k in list(flat_data):                             
+        flat_data = nested_qstring_keys(copy.deepcopy(json))
+        for k in list(flat_data):
             if hasattr(flat_data[k], 'read'): files[k] = flat_data.pop(k)
 
         if id: endpoint = os.path.join(endpoint, id)
@@ -51,7 +52,7 @@ class ARMRequest(object):
             convert_fieldmap(json, self.Object.field_map)
         params = nested_qstring_keys(params)
 
-        if files:   
+        if files:
             response = getattr(requests, method)(
                 urljoin(payload.api_url, endpoint.strip('/')),
                 params=params,
@@ -89,6 +90,8 @@ class ARMRequest(object):
                 or Error.http_code != response.status_code:
                     continue
                 raise Error(data.get('description'), data)
+            if response.status_code == 500:
+                raise payload.InternalServerError(data.get('description'), data)
             raise payload.BadRequest(data.get('description'), data)
 
     def get(self, id, **params):
