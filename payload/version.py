655081c82576c57a306baa07203d2882bc3bc219
Ian Halpern <ian@payload.co>
2023-07-18 12:08:53 -0400

    update version to pull from pyproject.toml file

--- payload/version.py
+++ payload/version.py
@@ -1,1 +1,14 @@
-__version__ = (0,4,4)
+import importlib.metadata
+from pathlib import Path
+
+try:
+    __version__ = importlib.metadata.version('payload-api')
+except importlib.metadata.PackageNotFoundError:
+    with open(str(Path(__file__).parent.parent)+'/pyproject.toml') as pyproject_toml:
+            __version__ = (
+                next(line for line in pyproject_toml if line.startswith("version"))
+                .split("=")[1]
+                .strip("'\"\n ")
+            )
+
+__version__ = tuple(map(int,__version__.split('.')))
